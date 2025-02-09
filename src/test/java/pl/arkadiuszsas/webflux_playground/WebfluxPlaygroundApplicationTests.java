c9fc04057f471bf1a3b366aba406347bb45cb33a
Arkadiusz Sas <kontakt.sas.arkadiusz@gmail.com>
2025-02-09 06:53:01 +0100

    Init the Project structure

---
+++ src/test/java/pl/arkadiuszsas/webflux_playground/WebfluxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arkadiuszsas.webflux_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
